e9a2e42335960044f9b8d8a3495785f0dff88739
Ankit Amonkar <ankit.amonkar@gmail.com>
2020-04-24 21:06:57 -0400

    Initial commit

---
+++ src/com/company/HanoiTower.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class HanoiTower {
+    private Peg peg1;
+    private Peg peg2;
+    private Peg peg3;
+    private int numDiscs;
+
+    public HanoiTower(int num)
+    {
+        // TODO implement constructor
+        peg1 = new Peg(1, num);
+        peg2 = new Peg(2, num);
+        peg3 = new Peg(3, num);
+    }
+
+    public void solveTower()
+    {
+        moveTower(peg1, peg3, peg2, numDiscs);
+    }
+
+    private void moveTower(Peg startPeg, Peg endPeg, Peg extraPeg, int numtoMove)
+    {
+        // TODO move discs(number input) from the start peg to the end peg
+        if(numtoMove==1){
+            startPeg.moveTopDisc(endPeg);
+        }
+        moveTower(startPeg, extraPeg, endPeg, numtoMove-1);
+        startPeg.moveTopDisc(endPeg);
+        moveTower(extraPeg, endPeg, startPeg, numtoMove-1);
+
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        HanoiTower ht = new HanoiTower(5);
+        ht.solveTower();
+    }
+
+}
---
+++ src/com/company/Peg.java
@@ -0,0 +1,95 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Peg {
+    private int pegNum;
+    private int numDiscs;
+    private int[] discs;
+    private int count;    //the number of discs currently on the peg.
+
+    public Peg(int p, int num)
+    {
+        pegNum = p;
+        numDiscs = num;
+        discs = new int[num];
+        count = 0;
+    }
+
+    public int getPegNum()
+    {
+        return pegNum;
+    }
+    public int getNumDiscs()
+    {
+        return numDiscs;
+    }
+
+    public String toString()
+    {
+        String s = "com.company.Peg " + pegNum;
+        return s;
+    }
+    public boolean addDisc(int whichOne)
+    {
+        if (discs[0]==0) {
+            discs[0] = whichOne;
+            count++;
+        }
+        // TODO add the disc with the given number to the peg.
+        // don't forget to make sure the move is value (no disc on top of a smaller one)
+        for(int i=0; i < discs.length; i++) {
+            if(discs[i]==0) {
+                if (whichOne > discs[i - 1]) {
+                    return false;
+                } else if (whichOne < discs[i - 1]) {
+                    discs[i] = whichOne;
+                    count++;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int popDisc()
+    {
+        // TODO remove the top disc from the peg.  Return its value.
+        for(int i=0; i<discs.length; i++){
+            if (i==discs.length || discs[i]!=0 && discs[i+1]==0){
+                int a = discs[i];
+                discs[i]=0;
+                count--;
+                return a;
+            }
+        }
+        return 0;
+    }
+
+    public boolean moveTopDisc(Peg newPeg)
+    {
+        int disc = popDisc();
+        if (newPeg.addDisc(disc))
+        {
+            System.out.println("Moving disc " + disc + " from peg " + pegNum + " to peg " + newPeg.getPegNum());
+            return true;
+        }
+        else{
+            System.out.println("Bad Move");
+            return false;
+        }
+    }
+    public static void main(String [] arg) {
+        Peg p1 = new Peg(1,5);
+        p1.addDisc(3);
+        System.out.println(Arrays.toString(p1.discs));
+        p1.addDisc(2);
+        System.out.println(Arrays.toString(p1.discs));
+        p1.addDisc(4);
+        System.out.println(Arrays.toString(p1.discs));
+        p1.popDisc();
+        System.out.println(Arrays.toString(p1.discs));
+    }
+
+
+}
