1f5b21f68e0cc8497e23ad51a493a87ed402dbf9
Ankit Amonkar <ankit.amonkar@gmail.com>
2020-04-26 22:28:09 -0400

    final

--- src/com/company/HanoiTower.java
+++ src/com/company/HanoiTower.java
@@ -42,3 +42,38 @@ public class HanoiTower {
     }
 
 }
+
+/*
+Results:
+Moving disc 1 from peg 1 to peg 3
+Moving disc 2 from peg 1 to peg 2
+Moving disc 1 from peg 3 to peg 2
+Moving disc 3 from peg 1 to peg 3
+Moving disc 1 from peg 2 to peg 1
+Moving disc 2 from peg 2 to peg 3
+Moving disc 1 from peg 1 to peg 3
+Moving disc 4 from peg 1 to peg 2
+Moving disc 1 from peg 3 to peg 2
+Moving disc 2 from peg 3 to peg 1
+Moving disc 1 from peg 2 to peg 1
+Moving disc 3 from peg 3 to peg 2
+Moving disc 1 from peg 1 to peg 3
+Moving disc 2 from peg 1 to peg 2
+Moving disc 1 from peg 3 to peg 2
+Moving disc 5 from peg 1 to peg 3
+Moving disc 1 from peg 2 to peg 1
+Moving disc 2 from peg 2 to peg 3
+Moving disc 1 from peg 1 to peg 3
+Moving disc 3 from peg 2 to peg 1
+Moving disc 1 from peg 3 to peg 2
+Moving disc 2 from peg 3 to peg 1
+Moving disc 1 from peg 2 to peg 1
+Moving disc 4 from peg 2 to peg 3
+Moving disc 1 from peg 1 to peg 3
+Moving disc 2 from peg 1 to peg 2
+Moving disc 1 from peg 3 to peg 2
+Moving disc 3 from peg 1 to peg 3
+Moving disc 1 from peg 2 to peg 1
+Moving disc 2 from peg 2 to peg 3
+Moving disc 1 from peg 1 to peg 3
+ */
